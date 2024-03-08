99e8027acfd957049d3b27b0d4e7bf79e2093b7e
Ramalingam-23 <110720984+Ramalingam-23@users.noreply.github.com>
2024-03-08 09:33:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,9 @@ import base64
 import sklearn as sk
 from streamlit.components.v1 import html
 
-loaded_model = pk.load(open("trained_model_lr.sav","rb"))
-scaled_data = pk.load(open("scaled_data.sav","rb"))
-
-
+# Load trained model and scaled data (replace with your filepaths)
+loaded_model = pk.load(open("trained_model_lr.sav", "rb"))
+scaled_data = pk.load(open("scaled_data.sav", "rb"))
 
 @st.cache_data
 def get_img_as_base64(file):
@@ -17,40 +16,53 @@ def get_img_as_base64(file):
         data = f.read()
     return base64.b64encode(data).decode()
 
-
-
-page_bg_img = f"""
-<style>
-[data-testid="stAppViewContainer"] > .main {{
-background-image: url("https://wallpapercave.com/wp/wp6696562.jpg");
-background-size: 100%;
-background-position: center;
-background-repeat: no-repeat;
-background-attachment: fixed;
-}}
-</style>
-"""
-
-st.markdown(page_bg_img, unsafe_allow_html=True)
-
-
-
-
-
-
-
+# Function to set background image dynamically based on selected engine size
+def set_background_image(engine_size):
+    background_images = {
+        0: "url('https://wallpapercave.com/wp/wp6696562.jpg')",
+        1: "url('https://hips.hearstapps.com/hmg-prod/images/8cc9595364efa0fc-org-1584048843.jpg?resize=1200:*')",
+        2: "url('https://global.toyota/pages/powertrain/engine/images/engine_001.png')",
+        3: "url('https://blogs.gomechanic.com/wp-content/uploads/2020/01/08b88a113768de393f3d95e87c1455e8.jpg')"
+    }
+    background_image = background_images.get(engine_size, "url('https://wallpapercave.com/wp/wp6696562.jpg')")
+    return f"""
+    <style>
+    [data-testid="stAppViewContainer"] > .main {{
+    background-image: {background_image};
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    }}
+    </style>
+    """
 
 def input_converter(inp):
-    vcl = ['Two-seater', 'Minicompact', 'Compact', 'Subcompact', 'Mid-size', 'Full-size', 'SUV: Small', 'SUV: Standard', 'Minivan', 'Station wagon: Small', 'Station wagon: Mid-size', 'Pickup truck: Small', 'Special purpose vehicle', 'Pickup truck: Standard']
-    trans = ['AV','AM','M','AS','A']
+    vehicle = [
+        "Two-seater",
+        "Minicompact",
+        "Compact",
+        "Subcompact",
+        "Mid-size",
+        "Full-size",
+        "SUV: Small",
+        "SUV: Standard",
+        "Minivan",
+        "Station wagon: Small",
+        "Station wagon: Mid-size",
+        "Pickup truck: Small",
+        "Special purpose vehicle",
+        "Pickup truck: Standard",
+    ]
+    transmission = ["AV", "AM", "M", "AS", "A"]
     fuel = ["D", "E", "X", "Z"]
     lst = []
     for i in range(6):
         if (type(inp[i]) == str):
-            if (inp[i] in vcl):
-                lst.append(vcl.index(inp[i]))
-            elif (inp[i] in trans):
-                lst.append(trans.index(inp[i]))
+            if (inp[i] in vehicle):
+                lst.append(vehicle.index(inp[i]))
+            elif (inp[i] in transmission):
+                lst.append(transmission.index(inp[i]))
             elif (inp[i] in fuel):
                 if (fuel.index(inp[i]) == 0):
                     lst.extend([1, 0, 0, 0])
@@ -70,70 +82,84 @@ def input_converter(inp):
     arr = arr.reshape(1, -1)
     arr = scaled_data.transform(arr)
     prediction = loaded_model.predict(arr)
-
-    return (f"The Fuel Consumption L/100km is {round(prediction[0], 2)}")
-
-
-
-
-
-
-
+    return (f"The Fuel Consumption km is {round(prediction[0], 2)}")
 
 def main():
     
-    #  title    
     
-    st.markdown("<h1 style='text-align: center; color: white;'>Fuel Consumption Prediction</h1>", unsafe_allow_html=True)        
-    # getting the input data from user    
+    # Title
+    st.markdown("<h1 style='text-align: center; color: white;'>Fuel Consumption Prediction</h1>", unsafe_allow_html=True)
+
+    # Input data from user
     result = 0
-    vehicle = ['Two-seater','Minicompact','Compact','Subcompact','Mid-size','Full-size','SUV: Small','SUV: Standard','Minivan','Station wagon: Small','Station wagon: Mid-size','Pickup truck: Small','Special purpose vehicle','Pickup truck: Standard']
-    transmission = ['AV','AM','M','AS','A']
+    vehicle = [
+        "Two-seater",
+        "Minicompact",
+        "Compact",
+        "Subcompact",
+        "Mid-size",
+        "Full-size",
+        "SUV: Small",
+        "SUV: Standard",
+        "Minivan",
+        "Station wagon: Small",
+        "Station wagon: Mid-size",
+        "Pickup truck: Small",
+        "Special purpose vehicle",
+        "Pickup truck: Standard",
+    ]
+    transmission = ["AV", "AM", "M", "AS", "A"]
     fuel = ["D", "E", "X", "Z"]
-    
 
-    Vehicle_class = st.selectbox(label = "Enter Vehicle class",options = vehicle)
-    
+    # Vehicle class dropdown
+    Vehicle_class = st.selectbox(label="Enter Vehicle class", options=vehicle)
+
+    # Styling for the dropdown (optional)
     css = """
-        <style>
-            .stSelectbox [data-testid='stMarkdownContainer'] {
-                color: white;
-            }
-            
-        </style>
+    <style>
+        .stSelectbox [data-testid='stMarkdownContainer'] {
+            color: white;
+        }
+    </style>
     """
-
     st.write(css, unsafe_allow_html=True)
-    
-    
-    Engine_size = st.selectbox("Select Engine Size (please enter value in this range[1-7])",options =[1,2,3,4,5,6,7])
-    css = """
-        <style>
-           .stNumberInput [data-testid='stMarkdownContainer'] {
-                color: white;
-                
-            }
-            
-        </style>
-    """
-    
+
+    # Engine size dropdown
+    Engine_size = st.selectbox(
+        "Select Engine Size (please enter value in this range [0-3])", options=[0 , 1, 2, 3]
+    )
+
+    # Styling for the dropdown (optional)
     st.write(css, unsafe_allow_html=True)
-    
-    Cylinders = st.number_input("Enter number of Cylinders (please enter value in this range[1-16]",min_value = 1, max_value = 16)
-    Transmission = st.selectbox("Select the Transmission",transmission)
-    Co2_Rating = st.number_input("Enter CO2 Rating (please enter value in this range[1-10]",min_value = 1, max_value = 10)
-    Fuel_type = st.selectbox("Select the Fuel type",fuel)
 
-    # code for prediction
+    # Cylinder input
+    Cylinders = st.number_input(
+        "Enter number of Cylinders (please enter value in this range [1-16])",
+        min_value=1,
+        max_value=16,
+    )
 
-    # creating a button for prediction
-    if st.button("Predict 🔍"):
-        result = input_converter([Vehicle_class,Engine_size,Cylinders,Transmission,Co2_Rating,Fuel_type])
+    # Transmission dropdown
+    Transmission = st.selectbox("Select the Transmission", transmission)
+
+    # CO2 Rating input
+    Co2_Rating = st.number_input(
+        "Enter CO2 Rating (please enter value in this range [1-10])",
+        min_value=1,
+        max_value=10,
+    )
+
+    # Fuel type dropdown
+    Fuel_type = st.selectbox("Select the Fuel type", fuel)
+
+    # Set background image based on selected engine size
+    st.markdown(set_background_image(Engine_size), unsafe_allow_html=True)
+
+    # Prediction button
+    if st.button("Predict "):
+        result = input_converter([Vehicle_class, Engine_size, Cylinders, Transmission, Co2_Rating, Fuel_type])
         markdown_text = f"<h2 style='color:white;'><b>{result}</b>!</h2>"
         st.markdown(markdown_text, unsafe_allow_html=True)
-
-#     st.success(result)
-
 
 if __name__ == "__main__":
     main()
